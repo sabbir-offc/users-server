d70d920f2a69f95bd10736d25b00194e6dd39ca7
sabbirofc <mdsabbirhowlader420@gmail.com>
2023-10-11 16:40:44 +0600

    post method created

---
+++ __steps__.js
@@ -0,0 +1,9 @@
+/**
+ * 1. create a post api in the server side.
+ * 2. client side send data via post.
+ * 3. set fetch method inside the fetch options (second params)
+ * 4. options will have three things: method
+ * 5. options will have headers: 'content-type':'application/json'
+ * 6. options will have body: don't forget to send data by JSON.stringify()
+ * 8. on the server side : don't forget to use express.json() middleware
+ */
